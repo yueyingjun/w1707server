e93f10c21fa215913201dd0fa6fc9727e80890d0
yueyingjuna <1046396106@qq.com>
2017-12-20 11:59:22 +0800

    server

--- server.js
+++ server.js
@@ -1,50 +1,89 @@
+#!/usr/bin/env node
 var http=require("http");
 var config=require("./config.json");
 var path=require("path");
 var fs=require("fs");
+const zlib = require('zlib');
+
 http.createServer(function(req,res){
+
     var url=req.url;
-    if(url=="/favicon.ico"){
-        fs.readFile("."+url,function(err,data){
-            res.setHeader("content-type","application/x-ico");
-            res.end(data);
-        })
-
-    }else{
-        console.log(req.url);
-        var root=path.resolve(config.root);
-        fs.readdir(root,function(err,data){
-            if(err){
-                res.writeHead(404,{"content-type":"text/html;charset=utf-8"});
-                res.end("根目录不存在");
-            }else{
-
-                if(path.extname(url)){
-                    var fullUrl=path.resolve(config.root,"."+url);
-
-                }else{
-                    var fullUrl=path.resolve(config.root,"."+url+"/"+config.index);
+    if(req.headers.cookie!=="name=zhangsan"&&req.headers.cookie!==undefined){
+        res.end("login");
+    }else {
+        if (url == "/favicon.ico") {
+            fs.readFile("." + url, function (err, data) {
+                if(err){
+                    res.end();
+                }else {
+                    res.setHeader("content-type", "application/x-ico");
+                    res.end(data);
                 }
+            })
 
-                fs.readFile(fullUrl,function(err,data){
-                    if(err){
-                        res.writeHead(404,{"content-type":"text/html;charset=utf-8"});
-                        res.end("页面不存在");
-                    }else{
+        } else {
 
-                        var extname=path.extname(fullUrl);
-                        res.writeHead(200,{"content-type":config.type[extname]+";charset=utf-8"});
-                      res.end(data);
+            var root = path.resolve(config.root);
+            fs.readdir(root, function (err, data) {
+                if (err) {
+                    res.writeHead(404, {"content-type": "text/html;charset=utf-8"});
+                    res.end("根目录不存在");
+                } else {
+                    if (path.extname(url)) {
+                        var fullUrl = path.resolve(config.root, "." + url);
+
+                    } else {
+                        var fullUrl = path.resolve(config.root, "." + url + "/" + config.index);
                     }
-                })
+
+                    fs.stat(fullUrl, function (err, info) {
+
+                        if (err){
+                            res.writeHead(404, {"content-type": "text/html;charset=utf-8"});
+                            res.end("页面不存在");
+                        } else {
+
+                            if(config["server-info"]){
+                                res.setHeader("server",config["server-info"]);
+                            }
+                            if(config["cache-control"]){
+                                res.setHeader("cache-control",config["cache-control"])
+                            }
+                            var time1 = ( new Date(req.headers["if-modified-since"]).getTime());
+                            var time2 = new Date(info.mtime).getTime();
+
+                            if (time1 && time1 == time2 &&config["last-modified"]) {
+                                res.writeHead(304);
+                                res.end();
+                            } else {
+                                if(config["last-modified"]) {
+                                    res.setHeader("Last-Modified", info.mtime.toUTCString());
+                                }
 
 
-            }
-        })
+                                var extname = path.extname(fullUrl);
+                                res.setHeader("set-cookie", "name=zhangsan");
+                                res.setHeader("Content-Encoding", "gzip");
 
+                                res.writeHead(200, {"content-type": config.type[extname] + ";charset=utf-8"});
+
+                                fs.createReadStream(fullUrl).pipe(zlib.createGzip()).pipe(res)
+
+
+                            }
+                        }
+                    })
+
+
+                }
+            })
+
+        }
     }
 
-}).listen(8888);
+}).listen(config.port,function(){
+    console.log(config.message);
+});
 
 
 
